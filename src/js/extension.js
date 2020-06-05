41e5c1b8e02fbb980c3c62eff5cb40233e093e16
Michael Sprague <mikesprague@gmail.com>
2020-06-05 10:00:41 -0400

    :poop: Add out for delivery updates
    Should refactor into one update status method

--- src/js/extension.js
+++ src/js/extension.js
@@ -17,6 +17,19 @@ const updateDelivered = () => {
   document.getElementsByTagName('head')[0].appendChild(linkTag);
 };
 
+const updateOutForDelivery = () => {
+  const linkTags = Array.from(document.querySelectorAll("link[rel*='icon']"));
+  linkTags.forEach(link => {
+    link.remove();
+  });
+  const linkTag = document.createElement('link');
+  linkTag.type = 'image/png';
+  linkTag.rel = 'shortcut icon';
+  linkTag.href = appConfig.favicons.outForDelivery;
+  window.document.title = `${appConfig.titlePrefix.outForDelivery} | ${window.document.title}`;
+  document.getElementsByTagName('head')[0].appendChild(linkTag);
+};
+
 const initExtension = () => {
   const deliveryService = currentService();
   let deliveryStatus = null;
@@ -48,6 +61,9 @@ const initExtension = () => {
     if (deliveryStatus === 'delivered') {
       updateDelivered();
       clearInterval(timerHandle);
+    }
+    if (deliveryStatus === 'out for delivery' || deliveryStatus === 'out for delivery today') {
+      updateOutForDelivery();
     }
   };
   setTimeout(doChecks, 5000);
