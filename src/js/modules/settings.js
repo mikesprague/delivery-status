3e66aa04ed87579f7970d33376fa7a1a41ee1199
Michael Sprague <mikesprague@gmail.com>
2020-06-22 16:29:44 -0400

    :bulb: Add some inline comments/docs

--- src/js/modules/settings.js
+++ src/js/modules/settings.js
@@ -1,5 +1,6 @@
 export const imagePath = 'https://raw.githubusercontent.com/mikesprague/delivery-status/master/src/images/';
 
+// selectors used to determine delivery status/where in html to add overlay
 export const selectors = {
   fedex: {
     deliveryStatus: '.snapshotController_addr_label.dest',
@@ -26,22 +27,26 @@ export const favicons = {
   main: `${imagePath}delivery-status-icon.png`,
 };
 
+// list of strings to look for that indicate current status
 export const statusStrings = {
   delivered: ['delivered'],
   inTransit: ['in transit', 'on its way', 'accepted', 'in-transit'],
   outForDelivery: ['out for delivery'],
 };
 
+// text will appear in title and on overlay
 export const titlePrefix = {
   delivered: 'DELIVERED',
   inTransit: 'In Transit',
   outForDelivery: 'Out For Delivery',
 };
 
+// class name used to change text color in overlay
 export const statusClass = {
   delivered: 'delivered',
   inTransit: 'in-transit',
   outForDelivery: 'out-for-delivery',
 };
 
+// how often to reload the window
 export const reloadInterval = 300000; // 5 minutes ((1000 * 60) * 5)
